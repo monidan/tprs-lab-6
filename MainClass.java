64ec2ee7e1817d61cacc034ab358beb02ab14f57
Daniel <horokhovbusiness@gmail.com>
2022-12-06 04:49:07 +0100

    fix unnecessary path details

--- MainClass.java
+++ MainClass.java
@@ -3,10 +3,9 @@ package com.SLOC;
 public class MainClass {
     public static void main(String[] args) {
         LOC locObj = new LOC();
-        String file = "C:\\Users\\aleks\\OneDrive\\Робочий стіл\\Lab_6_tprs\\src\\main\\java\\com\\SLOC\\LOC.java";
+        String file = "LOC.java";
 
         locObj.start(String.join(" ", file).trim());
         System.out.println(locObj.toString());
-
     }
 }
